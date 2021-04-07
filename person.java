29f3def507b437724b7a289045e060fa099c92bd
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2021-04-07 10:05:22 +0300

    Create person.java

---
+++ person.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Person {
+    String firstName;
+    String lastName;
+    String phone;
+
+    public Person(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+
+    }
+}
